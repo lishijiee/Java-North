f82f95acd8c85991befe03d51f6d76852161360c
lishijiee <2424782003@qq.com>
2022-08-22 22:24:51 +0800

    Java-North代码示例添加

---
+++ North-Code/src/main/java/com/lishijiee/NorthCodeApplication.java
@@ -0,0 +1,13 @@
+package com.lishijiee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NorthCodeApplication.class, args);
+    }
+
+}
